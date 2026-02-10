2b4a3bac9060f60abda8b1c2bfec197cc20f979d
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-10 17:58:13 +0530

    [saptadipa77]: Added Address Book DTO

---
+++ src/main/java/com/gevernova/AddressBook/dto/AddressBookDTO.java
@@ -0,0 +1,26 @@
+package com.gevernova.AddressBook.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+
+    @NotBlank(message = "Name cannot be empty")
+    @Pattern(
+            regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",
+            message = "Invalid name format"
+    )
+    private String name;
+
+    @NotBlank(message = "Address cannot be empty")
+    private String address;
+
+    @NotBlank(message = "Phone number required")
+    @Pattern(
+            regexp = "^[6-9][0-9]{9}$",
+            message = "Invalid phone number"
+    )
+    private String phone;
+}
+
