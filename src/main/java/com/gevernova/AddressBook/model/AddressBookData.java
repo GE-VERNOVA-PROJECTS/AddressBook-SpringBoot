8e9ee5804edecbb8a753f7584228f26992ff79a3
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-10 18:01:56 +0530

    [saptadipa77]: Added Address Book model

--- src/main/java/com/gevernova/AddressBook/model/AddressBookData.java
+++ src/main/java/com/gevernova/AddressBook/model/AddressBookData.java
@@ -1,19 +1,18 @@
 package com.gevernova.AddressBook.model;
+
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "address_book")
 public class AddressBookData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String address;
     private String phone;
-
-    public AddressBookData(int id, String name, String address, String phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
 }
-
